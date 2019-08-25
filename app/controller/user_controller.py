e7c2d14c908129f9dc6eec675a9a64d724cd7fef
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-08-25 01:25:28 -0300

    Clean code

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -3,5 +3,4 @@ from sanic.response import json
 def setup_routes(app):
     @app.route("/")
     async def index(request):
-        print('daniel')
         return json({"hello": "world"})
--- app/tests/controller/test_user_controller.py
+++ app/tests/controller/test_user_controller.py
@@ -1,4 +1,11 @@
-async def test_pytest_test_client(test_cli):
-    resp = await test_cli.get('/')
-    assert resp.status == 200
-    assert (await resp.json()) == {"hello": "world"}
+from main import app
+
+def test_index_returns_200():
+    request, response = app.test_client.get('/')
+    assert response.status == 200
+    assert (response.json) == {"hello": "world"}
+
+
+def test_index_put_not_allowed():
+    request, response = app.test_client.put('/')
+    assert response.status == 405
